f23fb72e7ec738b245f70449046fd2424eaf565a
Ali Furkan Çağlayan <afurkancaglayan07@gmail.com>
2022-09-17 10:33:15 +0300

    Add project files.

--- DataAccess/Concrete/EntityFramework/NortwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NortwindContext.cs
@@ -14,11 +14,12 @@ namespace DataAccess.Concrete.EntityFramework
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-0B3NF7O;Initial Catalog=Northwind;Integrated Security=true;Connect Timeout=30;Encrypt=false;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-            optionsBuilder.UseSqlServer(@"Server=DESKTOP-0B3NF7O;Database=Northwind;User Id=;Password=;");
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-0B3NF7O;Database=Northwind;User Id=sa;Password=123456;");
         }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }
